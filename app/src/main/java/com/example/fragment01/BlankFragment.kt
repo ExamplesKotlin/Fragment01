782525bb8aeca43fca2beb47eac6deb5906b18d1
Henry Ambicho T <yiperu@gmail.com>
2019-11-18 23:26:43 -0300

    Add new Fragment and manage this with supportFragmentManager

--- app/src/main/java/com/example/fragment01/BlankFragment.kt
+++ app/src/main/java/com/example/fragment01/BlankFragment.kt
@@ -1,21 +1,24 @@
 package com.example.fragment01
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class HelloFragment : Fragment() {
+/**
+ * A simple [Fragment] subclass.
+ */
+class BlankFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val view = inflater.inflate(R.layout.fragment_hello, container, false)
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
     }
+
 
 }
--- app/src/main/java/com/example/fragment01/MainActivity.kt
+++ app/src/main/java/com/example/fragment01/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val fragment = BlankFragment()
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.add(R.id.container, fragment, "BlankFragment")
+        transaction.commit()
+
     }
 }
